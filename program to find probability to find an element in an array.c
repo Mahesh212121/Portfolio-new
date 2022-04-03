de31602559baa63896785457e7f026a7951379d8
Mahesh <itsmaheshyadav2106@gmail.com>
2022-04-03 17:17:10 +0530

    First

---
+++ program to find probability to find an element in an array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAX_SIZE 1000 
+
+int main()
+{
+    int arr[MAX_SIZE];
+    int i, N,X;
+    float a=0,b;
+    printf("Enter the size of array\n");
+    scanf("%d", &N);
+    for(i=0; i<N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the element to search\n");
+    scanf("%d",&X);
+    for(i=0; i<N; i++)
+    {
+        if(arr[i]==X)
+        {
+            a=a+1;
+        }
+    }
+    b=a/N;
+    printf("The probability to find the given element is %f ", b);
+
+    return 0;
+}
---
+++ tute.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main ()
+{
+    int a=8;
+    float b=7.333;
+    printf("Hello World\n");
+    printf("%10.4f\n",b);
+    
+    return 0;
+    
+}
